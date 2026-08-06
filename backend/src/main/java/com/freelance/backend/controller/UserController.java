14c753ee1a10d30d299b47323d022ee916b63f8d
dhruv49k <dhruvkathale@gmail.com>
2026-08-06 19:54:20 +0530

    Add user registration controller

---
+++ backend/src/main/java/com/freelance/backend/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.freelance.backend.controller;
+
+import com.freelance.backend.entity.User;
+import com.freelance.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+
+        User savedUser = userService.registerUser(user);
+
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+}
